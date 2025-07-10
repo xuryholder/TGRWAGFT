45bd5cf35f62aaa76e0fa9dd21ff99b7a5ea56db
Denis <denys.stepanov@gmail.com>
2025-07-10 13:44:39 +0700

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+
+import React, { useEffect } from 'react';
 import { TonConnectButton, TonConnectUIProvider } from '@tonconnect/ui-react';
 
+
 function App() {
+  useEffect(() => {
+    if (window.Telegram && window.Telegram.WebApp) {
+      window.Telegram.WebApp.ready();
+      // Можно добавить дополнительную логику взаимодействия с Telegram WebApp API
+    }
+  }, []);
+
   return (
-    <TonConnectUIProvider manifestUrl="https://your-server.com/tonconnect-manifest.json">
+    <TonConnectUIProvider manifestUrl="https://tgrwagft.fly.dev/tonconnect-manifest.json">
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
         <h1>Telegram Mini App</h1>
         <TonConnectButton />
